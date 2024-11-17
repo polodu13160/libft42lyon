e2252d6d79ed38a2253cca96637129a005139fcf
Polo13160 <paul.depetrini@gmail.com>
2024-11-17 22:16:38 +0100

    iota de mes mort

---
+++ ft_itoa.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pde-petr <pde-petr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 19:27:21 by pde-petr          #+#    #+#             */
+/*   Updated: 2024/11/17 22:15:55 by pde-petr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_tab(int n)
+{
+	int	neg;
+	int	count;
+
+	neg = 0;
+	if (n < 0)
+	{
+		neg = 1;
+		n = -n;
+	}
+	count = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count + neg);
+}
+
+static void	add_to_array(int n, char *tab)
+{
+	int	neg;
+	int	i;
+
+	neg = 0;
+	if (n < 0)
+	{
+		neg = 1;
+		n = -n;
+	}
+	i = 0;
+	if (n == 0)
+		tab[i] = 48;
+	while (n != 0)
+	{
+		tab[i] = n % 10 + 48;
+		n /= 10;
+		i++;
+	}
+	if (neg == 1)
+		tab[i++] = '-';
+	tab[++i] = '\0';
+}
+
+static void	reverse_tab(char *tab, int count)
+{
+	int		i;
+	char	temp;
+
+	i = 0;
+	count--;
+	while (i < count)
+	{
+		temp = tab[i];
+		tab[i] = tab[count];
+		tab[count] = temp;
+		i++;
+		count--;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*tab;
+	int		count;
+	int		i;
+
+	if (n == -2147483648)
+	{
+		tab = ft_calloc(12, sizeof(char));
+		if (!tab)
+			return (NULL);
+		ft_strlcpy(tab, "-2147483648", 12);
+		return (tab);
+	}
+	else
+	{
+		count = count_tab(n);
+		tab = ft_calloc(count, sizeof(char));
+		add_to_array(n, tab);
+		reverse_tab(tab, count);
+	}
+	return (tab);
+}
+
+int	main(void)
+{
+	int a = 0;
+	printf("%s\n", ft_itoa(a));
+	return (0);
+}
